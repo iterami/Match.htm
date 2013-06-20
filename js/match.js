fcb12bc96eb283283420b081e04ca6e9288596aa
Jan Horava <czechinthemail@gmail.com>
2013-06-19 19:06:17 -0700

    improved formatting

--- js/match.js
+++ js/match.js
@@ -2,6 +2,7 @@ function button_click(button_id){
     /*set button color and value*/
     get(button_id).classList.remove('color10');
     get(button_id).classList.add('color' + button_values[button_id]);
+
     get(button_id).value = [
         'ABCDEFGHIJ'[button_values[button_id]],
         button_values[button_id],
@@ -15,7 +16,7 @@ function button_click(button_id){
             if(button_values[i] > -1 && i !== button_id){
                 get(i).classList.remove('color' + button_values[i]);
                 get(i).classList.add('color10');
-                get(i).value = '-'
+                get(i).value = '-';
             }
         }while(i--);
 
@@ -26,7 +27,7 @@ function button_click(button_id){
         ];
 
         /*disable this button to prevent reclicks*/
-        get(button_id).disabled = 1
+        get(button_id).disabled = 1;
     }else{
         get('attempted-matches').innerHTML = parseInt(get('attempted-matches').innerHTML) + 1;
 
@@ -42,7 +43,7 @@ function button_click(button_id){
         do{
             get(i).disabled = button_values[i] < 0 ? 1 : 0;
             if(!get(i).disabled){
-                x = 0
+                x = 0;
             }
         }while(i--);
 
@@ -53,7 +54,7 @@ function button_click(button_id){
 
         /*if not, end game*/
         if(x){
-            stop()
+            stop();
         }
     }
 }
@@ -64,19 +65,19 @@ function decisecond(){
 
     /*if time can run out, check if game over*/
     if(get('max-time').value > 0 && get('time').innerHTML <= 0){
-        stop()
+        stop();
     }
 }
 
 function get(i){
-    return document.getElementById(i)
+    return document.getElementById(i);
 }
 
 function play_audio(i){
     if(get('audio-volume').value > 0){
         get(i).volume = get('audio-volume').value;
         get(i).currentTime = 0;
-        get(i).play()
+        get(i).play();
     }
 }
 
@@ -87,7 +88,8 @@ function reset(){
         get('max-time').value = 0;
         get('start-key').value = 'H';
         get('y-margin').value = 0;
-        save()
+
+        save();
     }
 }
 
@@ -102,19 +104,22 @@ function save(){
         'start-key'
     ];
     do{
-        if(get(j[i]).value == [1,0,0,1,'H'][i]){
-            ls.removeItem('match' + i)
+        if(get(j[i]).value == [1, 0, 0, 1, 'H'][i]){
+            ls.removeItem('match-' + i);
         }else{
-            ls.setItem('match' + i,get(j[i]).value)
+            ls.setItem(
+                'match-' + i,
+                get(j[i]).value
+            );
         }
     }while(i--);
-    j = 0
+    j = 0;
 }
 
 function showhide(){
     i = get('showhide-button').value === '-' ? 1 : 0;
-    get('settings-span').style.display = ['inline','none'][i];
-    get('showhide-button').value = ['-','+'][i]
+    get('settings-span').style.display = ['inline', 'none'][i];
+    get('showhide-button').value = ['-', '+'][i];
 }
 
 function start(){
@@ -127,7 +132,11 @@ function start(){
     ];
     do{
         if(isNaN(get(j[i]).value) || get(j[i]).value < 0){
-            get(j[i]).value = [1,0,0][i]
+            get(j[i]).value = [
+                1,
+                0,
+                0
+            ][i];
         }
     }while(i--);
 
@@ -143,22 +152,24 @@ function start(){
 
         j = 9;
         do{
-            get(i).classList.remove('color' + j)
+            get(i).classList.remove('color' + j);
         }while(j--);
 
         get(i).classList.add('color10');
         get(i).value = '-';
 
         do{
-            temp = Math.floor(Math.random() * 20)
+            temp = Math.floor(Math.random() * 20);
         }while(tempinfo[temp] != -1);
 
         tempinfo[temp] = Math.floor(temp / 2);
-        button_values[i] = Math.floor(temp / 2)
+        button_values[i] = Math.floor(temp / 2);
     }while(i--);
 
     get('start-button').value = 'End (ESC)';
-    get('start-button').onclick = function(){stop()};
+    get('start-button').onclick = function(){
+        stop();
+    };
 
     /*disable settings to prevent editing*/
     get('display-select').disabled = 1;
@@ -168,13 +179,13 @@ function start(){
     /*display time limit if it is greater than 0*/
     if(get('max-time').value > 0){
         get('time').innerHTML = get('time-max').innerHTML = get('max-time').value;
-        get('if-time-limit').style.display = 'inline'
+        get('if-time-limit').style.display = 'inline';
     }else{
         get('time').innerHTML = 0;
-        get('if-time-limit').style.display = 'none'
+        get('if-time-limit').style.display = 'none';
     }
 
-    interval = setInterval('decisecond()',100);
+    interval = setInterval('decisecond()', 100);
     save()
 }
 
@@ -182,7 +193,9 @@ function stop(){
     clearInterval(interval);
 
     get('start-button').value = 'Start (' + get('start-key').value + ')';
-    get('start-button').onclick = function(){start()};
+    get('start-button').onclick = function(){
+        start();
+    };
 
     /*disable all game-area buttons*/
     i = 19;
@@ -203,12 +216,12 @@ function stop(){
         -1,-1,-1,-1,-1,
         -1,-1,-1,-1,-1
     ];
-    selected_buttons = [-1,-1];
+    selected_buttons = [-1, -1];
 
     /*enable settings to allow editing*/
     get('display-select').disabled = 0;
     get('max-time').disabled = 0;
-    get('reset-button').disabled = 0
+    get('reset-button').disabled = 0;
 }
 
 var button_values = [
@@ -232,25 +245,25 @@ var tempinfo = [
 /*setup buttons in game-area*/
 for(i = 0; i < 20; i++){
     if(i % 5 === 0 && i !== 0){
-        j.push('<br>')
+        j.push('<br>');
     }
-    j.push('<input class="buttons color10" disabled id=' + i + ' onclick=button_click(' + i + ') type=button value=->')
+    j.push('<input class="buttons color10" disabled id=' + i + ' onclick=button_click(' + i + ') type=button value=->');
 }
 get('game-area').innerHTML = j.join('');
 j = 0;
 
 /*setup settings, get values from localStorage if they are set*/
-get('audio-volume').value = ls.getItem('match0') === null ? 1 : parseFloat(ls.getItem('match0'));
-get('display-select').value = ls.getItem('match3') === null ? 1 : 0;
-get('max-time').value = ls.getItem('match1') === null ? 0 : parseInt(ls.getItem('match1'));
-get('y-margin').value = ls.getItem('match2') === null ? 0 : parseInt(ls.getItem('match2'));
+get('audio-volume').value = ls.getItem('match-0') === null ? 1 : parseFloat(ls.getItem('match-0'));
+get('display-select').value = ls.getItem('match-3') === null ? 1 : 0;
+get('max-time').value = ls.getItem('match-1') === null ? 0 : parseInt(ls.getItem('match-1'));
+get('y-margin').value = ls.getItem('match-2') === null ? 0 : parseInt(ls.getItem('match-2'));
 
 /*set value of start-key if saved into localStorage*/
-if(ls.getItem('match4') === null){
-    get('start-key').value = 'H'
+if(ls.getItem('match-4') === null){
+    get('start-key').value = 'H';
 }else{
-    get('start-key').value = ls.getItem('match4');
-    get('start-button').value = 'Start (' + ls.getItem('match4') + ')'
+    get('start-key').value = ls.getItem('match-4');
+    get('start-button').value = 'Start (' + ls.getItem('match-4') + ')';
 }
 
 /*set y margin of table based on settings*/
@@ -262,8 +275,9 @@ window.onkeydown = function(e){
 
     if(String.fromCharCode(i) === get('start-key').value){
         stop();
-        start()
+        start();
+
     }else if(i === 27){/*ESC*/
-        stop()
+        stop();
     }
 }
