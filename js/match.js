b15ad1c49d8349c7d75ff8efbedd8478d53db58e
Jan Horava <czechinthemail@gmail.com>
2015-02-16 19:52:11 -0800

    improved clarity

--- js/match.js
+++ js/match.js
@@ -132,14 +132,14 @@ function save(){
     }while(loop_counter--);
 }
 
-function showhide(){
-    if(document.getElementById('showhide-button').value === '-'){
+function settings_toggle(){
+    if(document.getElementById('settings-button').value === '-'){
         document.getElementById('settings-span').style.display = 'none';
-        document.getElementById('showhide-button').value = '+';
+        document.getElementById('settings-button').value = '+';
 
     }else{
         document.getElementById('settings-span').style.display = 'inline';
-        document.getElementById('showhide-button').value = '-';
+        document.getElementById('settings-button').value = '-';
     }
 }
 
